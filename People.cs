942b8bce72cf8b86ed452f63bda69459e3159788
ducnt12 <hello.there.two.nine@gmail.com>
2022-10-22 13:44:35 +0700

    Initial commit

---
+++ People.cs
@@ -0,0 +1,20 @@
+class People
+{
+    private int age;
+    private double height;
+    public People(int age, double height)
+    {
+        this.age = age;
+        this.height = height;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public double getHeight()
+    {
+        return this.height;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# C# Intro
+C# is an object-oriented programming language which is similar to Java the most in terms of syntax and logic.
+## CMD commands
+- Create new c# project: `dotnet new console` 
+- Run `C#` projects: `dotnet run`
